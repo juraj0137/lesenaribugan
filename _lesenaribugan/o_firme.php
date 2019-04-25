053cdf70e0c756113f0112c9ffed19d65bdee2f6
Juraj Kubala <juraj.alabuk@gmail.com>
2019-04-25 19:49:39 +0200

    Corrected diacritics

--- _lesenaribugan/o_firme.php
+++ _lesenaribugan/o_firme.php
@@ -18,7 +18,7 @@
 		<h2>Vítejte u nás</h2>
 		<img class="img-frame" src="./img/lesenar.jpg">
 		<p>Naše společnost provádí speciální lešenářské práce, práce ve výškách, malířské práce, zednické práce a mnoho jiného. Naši zkušení pracovníci kompletně zajistí stavbu lešení, dovoz a odvoz materiálu ze stavby. Pracujeme s různými typy lešení v závislosti na typu a charakteru požadavek.</p>
-		<p>Pro stavbu pracujeme s <b>trubkovým lešením</b>, lešením <b>Layher</b>,<b> Sprint</b> a <b>Peri</b>. Naši dominantou je <b>trubkové lešení</b>, které montujeme jako:
+		<p>Pro stavbu pracujeme s <b>trubkovým lešením</b>, lešením <b>Layher</b>,<b> Sprint</b> a <b>Peri</b>. Lešení montujeme jako:
 			<ul>
 				<li>Závěsné  lešení</li>
 				<li>Řádově  lešení</li>
